2206a2f3e3e436a7395441733d5a8c0e71690a98
Fiona D'Mello <40391218+FionaDmello@users.noreply.github.com>
2026-04-02 16:59:58 +0200

    some content added

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import './App.css'
 function App() {
 
   return (
-    <>
+    <div className="bg-linear-to-br from-[#F9FAFB] to-[#D2D6DB] min-h-screen flex flex-col p-4">
+      <div className="flex-1 rounded bg-white shadow gap-2 px-4 py-12">
+        <h1>Frequently asked questions</h1>
+        <p>Get all your questions answered</p>
+      </div>
+      
       <div className="credits">
         A challenge by
         <a
@@ -15,7 +20,7 @@ function App() {
           >Your Name</a
         >.
       </div>
-    </>
+    </div>
   )
 }
 
