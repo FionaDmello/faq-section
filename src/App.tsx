dc1a724a8b8c8778994a3664c3be270e37c75509
Fiona D'Mello <40391218+FionaDmello@users.noreply.github.com>
2026-09-02 13:30:20 +0200

    styled application for md screens

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
 
   return (
     <div className="bg-linear-to-br from-[#F9FAFB] to-[#D2D6DB] min-h-screen flex flex-col p-4">
-      <div className="flex-1 rounded bg-white shadow gap-2 px-4 py-12 md:py-18">
-        <div className="space-y-12 md:space-y-18">
+      <div className="flex-1 rounded md:rounded-1.5 bg-white shadow gap-2 px-4 py-12 md:py-16 items-center">
+        <div className="space-y-12 md:space-y-16">
           <div className="space-y-5 text-center">
             <h1 className="text-3xl/7.5 md:text-5xl/12 font-semibold">Frequently asked questions</h1>
             <p className="text-lg/4.5 md:text-xl/4 text-neutral-600">Get all your questions answered</p>
@@ -34,9 +34,9 @@ function App() {
                 )
               })
             }
-            <div className="border-2 border-neutral-200 shadow-md rounded-lg p-4 space-y-4 flex flex-col">
+            <div className="border-2 border-neutral-200 shadow-md rounded-lg p-4 md:p-8 gap-y-4 md:gap-y-0 md:gap-x-4 flex flex-col md:flex-row md:justify-between md:items-center">
               <div className="space-y-2">
-                <h4 className="text-2xl font-semibold text-left">Can't find the answer you're looking for?</h4>
+                <h4 className="text-xl font-semibold text-left">Can't find the answer you're looking for?</h4>
                 <p className="text-base text-neutral-600">
                   Reach out to our <span><a className="text-indigo-700  hover:text-indigo-800 focus:outline-4 focus:outline-offset-1 focus:outline-indigo-200 disabled:text-neutral-400 rounded">customer support</a></span> team.
                 </p>
